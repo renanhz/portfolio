a367affed02da8d797a51135f477d9e0f3d57af2
renan <renan.stos077@gmail.com>
2023-12-20 16:38:23 -0300

    changing matrix-effect animation

--- js/matrix-effect.js
+++ js/matrix-effect.js
@@ -1,38 +1,85 @@
-// Initialising the canvas
-let canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d');
-
-// Setting the width and height of the canvas
+//https://youtu.be/f5ZswIE_SgY
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Setting up the letters
-let letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZアァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters = 'ABCDEFGHIJKLMNOPQRSTUVXYZアァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = '';
+    this.canvasHeight = canvasHeight;
+  }
 
-// Setting up the columns
-let fontSize = 16,
-    columns = canvas.width / fontSize;
-
-// Setting up the drops
-let drops = [];
-for (var i = 0; i < columns; i++) {
-  drops[i] = 1;
-}
-
-// Setting up the draw function
-function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < drops.length; i++) {
-    let text = letters.charAt(Math.floor(Math.random() * letters.length));
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
+  draw(context) {
+    this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.9) {
+      this.y = 0;
+    } else {
+      this.y+=1;
     }
   }
 }
 
-// Loop the animation
-setInterval(draw, 33);
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 25;
+    this.columns = this.canvasWidth/this.fontSize;
+    this.symbols = [];
+
+    this.#initialize();
+  }
+
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+    }
+  }
+
+  resize(width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth/this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 30;
+const nextFrame = 1000/fps;
+let timer = 0;
+
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+
+  if (timer > nextFrame) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.textAlign = 'center';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0aff0a';
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate(0);
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+});
